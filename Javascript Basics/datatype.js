fcba13bd6024d57e3f7c37950a7b7f93ad028a7d
skmdJeesan <skmdjeesan@gmail.com>
2025-06-20 15:39:42 +0000

    typeof undefined, null, array & obj

--- Javascript Basics/datatype.js
+++ Javascript Basics/datatype.js
@@ -3,11 +3,16 @@ let b = 3.14;
 let c = "Jeesan";
 let d = true;
 let e = 'q';
+let x;
+let y = null;
+let z = ['abc', 69, {name: 'Jeesan', rno: 1026,}, true];
 
-console.log(a,typeof a);
-console.log(b,typeof b);
-console.log(c,typeof c);
-console.log(d,typeof d);
-console.log(e,typeof e);
+console.log(a,typeof a); // number
+console.log(b,typeof b); // number
+console.log(c,typeof c); // string
+console.log(d,typeof d); // boolean
+console.log(e,typeof e); // string
+console.log(x,typeof x); // undefined
+console.log(y, typeof y); //object
+console.log(z, typeof z); //object
 
-console.log('hello world');
